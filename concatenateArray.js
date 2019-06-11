37e669f4fa9335d996d1a00289f07a989d11fc87
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-11 15:05:35 +0000

    Used concat function to combine arrays

---
+++ concatenateArray.js
@@ -0,0 +1,11 @@
+function concat(arrayOne, arrayTwo) {
+  let combinedArray = arrayOne.concat(arrayTwo);
+
+  return combinedArray;
+}
+
+// Default tests to check that function works
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
